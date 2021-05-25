aa5376ba516e594d2aa4132888f24e5ae64d2b8d
huihuijiangqiang <59404531+huihuijiangqiang@users.noreply.github.com>
2021-05-25 14:43:52 +0800

    责任链

---
+++ src/DecoratorPatternMain.java
@@ -0,0 +1,19 @@
+import com.DecoratorPattern.ReadEnglishWord;
+import com.DecoratorPattern.WordDecorator;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class DecoratorPatternMain {
+
+    public static void main(String[] args) {
+        ReadEnglishWord readEnglishWord = new ReadEnglishWord();
+        WordDecorator wordDecorator1 = new WordDecorator(readEnglishWord, new File( "D:\\github\\untitled2\\src\\w.txt"));
+        ArrayList<String> wordList = wordDecorator1.readWords(new File( "D:\\github\\untitled2\\src\\w1.txt"));
+        for (String s : wordList) {
+            System.out.println(s);
+        }
+
+
+    }
+}
